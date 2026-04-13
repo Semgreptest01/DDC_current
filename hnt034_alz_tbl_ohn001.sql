82225919b93a763977892dab047172a0e76dc80a
Semgreptest01 <d.yuuta.watanabe.bsa@law001.lawson.co.jp>
2026-04-13 15:56:28 +0900

    Add files via upload

---
+++ hnt034_alz_tbl_ohn001.sql
@@ -0,0 +1,3 @@
+WHENEVER SQLERROR EXIT SQL.SQLCODE ROLLBACK
+@/wk_apl/apl/prod/ora10/table/hnt034_alz_tbl.sql
+EXIT
---
+++ hnt040_alz_tbl_ohn001.sql
@@ -0,0 +1,3 @@
+WHENEVER SQLERROR EXIT SQL.SQLCODE ROLLBACK
+@/wk_apl/apl/prod/ora10/table/hnt040_alz_tbl.sql
+EXIT
